df0ddcbe85d0b41572a9989a63220f7f3f72482b
Semir <semko2m@gmail.com>
2015-03-03 10:31:32 +0100

    firs comit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="125 S. Bloomingdale Rd., Suite #11"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Bloominglade, IL 60108";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2246538094;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="slideup"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
